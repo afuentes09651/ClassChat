bf9bd26d0a2a8a3d690e7f8ab1f950a720d628f0
Armando <armandez09651@yahoo.com>
2020-10-31 13:18:48 -0500

    basic split test

--- src/com/company/SplitTest.java
+++ src/com/company/SplitTest.java
@@ -1,4 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class SplitTest {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter string to split: ");
+
+        String x = reader.nextLine();
+        String[] strings;
+
+        if (x.indexOf(':') != -1) {
+            strings = x.split(":", 2);
+        } else {
+            x = "all:" + x;
+            strings = x.split(":", 2);
+        }
+
+
+        System.out.println(strings[0]);
+        System.out.println(strings[1]);
+    }
 }
